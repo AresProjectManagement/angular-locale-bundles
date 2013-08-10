f04576e2c21ca9f11bf9b0b494f08f741a0e1d01
wmluke <wmlukeb@gmail.com>
2013-08-10 00:20:07 -0700

    Upgraded karma and added text code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,55 +1,80 @@
 // Karma configuration
+// Generated on Fri Aug 09 2013 09:44:05 GMT-0700 (PDT)
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function (config) {
+    'use strict';
 
-// list of files / patterns to load in the browser
-files = [
-    JASMINE,
-    JASMINE_ADAPTER,
-    'app/bower_components/angular/angular.js',
-    'app/bower_components/angular-mocks/angular-mocks.js',
-    'src/*.js',
-    'test/mock/**/*.js',
-    'test/spec/**/*.js'
-];
+    config.set({
 
-// list of files to exclude
-exclude = [];
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+        // frameworks to use
+        frameworks: ['jasmine'],
 
-// web server port
-port = 8080;
+        // list of files / patterns to load in the browser
+        files: [
+            'app/bower_components/angular/angular.js',
+            'app/bower_components/angular-mocks/angular-mocks.js',
+            'src/*.js',
+            'test/mock/**/*.js',
+            'test/spec/**/*.js'
+        ],
 
-// cli runner port
-runnerPort = 9100;
+        // list of files to exclude
+        exclude: [],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+        // test results reporter to use
+        // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+        reporters: ['progress', 'junit', 'coverage'],
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+        preprocessors: {
+            // source files, that you wanna generate coverage for
+            // do not include tests or libraries
+            // (these files will be instrumented by Istanbul)
+            'src/*.js': ['coverage']
+        },
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+        coverageReporter: {
+            type: 'text-summary',
+            dir: 'reports/coverage'
+            //file: 'unit-test-coverage.html'
+        },
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['PhantomJS'];
+        junitReporter: {
+            outputFile: 'reports/test/unit-test-results.xml'
+        },
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+        // web server port
+        port: 8181,
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+        runnerPort: 9100,
+
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_INFO,
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
+
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['PhantomJS'],
+
+        // If browser does not capture in given timeout [ms], kill it
+        captureTimeout: 5000,
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false
+    });
+};
